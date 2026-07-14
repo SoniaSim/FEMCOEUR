794e9d51c4969e3456ea0d4f0ecb1d5fd844abc8
Sonia Si Moussi <sonia.simoussi@now-coworking.com>
2026-07-14 16:48:18 +0200

    feat: ✨ add sanity-plugin-media for enhanced media handling in Sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { frFRLocale } from "@sanity/locale-fr-fr";
+import { media } from "sanity-plugin-media";
 import { schemaTypes } from "./sanity/schema";
 import { structure } from "./sanity/structure";
 
@@ -10,7 +11,7 @@ export default defineConfig({
   basePath: "/studio",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  plugins: [structureTool({ structure }), frFRLocale()],
+  plugins: [structureTool({ structure }), media(), frFRLocale()],
   schema: {
     types: schemaTypes,
   },
